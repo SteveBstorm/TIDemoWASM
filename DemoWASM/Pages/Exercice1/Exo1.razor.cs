c8916a2f8aa0447e047e077423fe3b5356ef2f92
stevebstorm <steve.lorent@gmail.Com>
2025-01-06 16:16:54 +0100

    Correctif Exo1

---
+++ DemoWASM/Pages/Exercice1/Exo1.razor.cs
@@ -0,0 +1,21 @@
+﻿namespace DemoWASM.Pages.Exercice1
+{
+    public partial class Exo1
+    {
+        public string Username { get; set; }
+
+        private List<string> Reponses { get; set; } = new List<string>();
+        public bool GameIsOver { get; set; }
+
+        public void EnregistrerReponse(string reponse)
+        {
+            Reponses.Add(reponse);
+        }
+
+        private void ChangeGameStatus()
+        {
+            GameIsOver = true;
+        }
+
+    }
+}
---
+++ DemoWASM/Pages/Exercice1/Quizz.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DemoWASM.Pages.Exercice1
+{
+    public partial class Quizz
+    {
+        public List<string> Questions { get; set; } = new List<string>();
+        public int Compteur { get; set; } = 0;
+
+        [Parameter]
+        public EventCallback<string> NotifyResponse { get; set; }
+        [Parameter]
+        public EventCallback NotifyEndGame { get; set; }
+
+        [Parameter]
+        public string Pseudo { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Questions.Add("Question 1");
+            Questions.Add("Question 2");
+            Questions.Add("Question 3");
+        }
+
+        public void Repondre(string rep)
+        {
+            NotifyResponse.InvokeAsync(rep);
+            Compteur++;
+            if(Compteur >= Questions.Count)
+            {
+                NotifyEndGame.InvokeAsync();
+            }
+        }
+    }
+}
